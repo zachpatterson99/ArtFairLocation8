bab1945b3ad0c09a75eac6fd6a53bee4c2755067
zachpatterson99 <zachpatterson99@gmail.com>
2024-07-05 16:28:01 -0400

    commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-location-8--state--madison",
+      "name": "Location #8- State & Madison",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3417986173854253,
+        "pitch": 0.046287793679034905,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5788386929931306,
+          "pitch": -0.14662349505116623,
+          "title": "Emergency Location",
+          "text": "<h1 class=\"DUwDvf lfPIob\" style=\"background: rgb(255, 255, 255); border: 0px; border-radius: 0px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; font-size: 1.375rem; line-height: 1.75rem; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; list-style: none; outline: 0px; overflow: visible; vertical-align: baseline; color: rgb(32, 33, 36);\">Law Quadrangle</h1>"
+        }
+      ]
+    }
+  ],
+  "name": "Location #8 - State & Madison",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
